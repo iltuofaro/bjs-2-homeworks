bab0daf1f4b83f110832debb14ef8a6b0cae0980
And8Alex <sand.dreeva@gmail.com>
2022-01-17 17:06:54 +0700

    need more practice..

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,42 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = Math.pow(b, 2) - (4 * a * c);
+
+  if (d > 0) {
+    arr.push( (-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a) );
+    // arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    // arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+    // arr = [- b / (2 * a)];
+  } 
+
+  return arr;
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
+  // контроль на корректность введенных данных
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит некорректное значение: ${percent}`;
+  } else if (isNaN(contribution)) {
+    return `Параметр "Первоначальный взнос" содержит некорректное значение: ${contribution}`;
+  } else if (isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит некорректное значение: ${amount}`;
+  }
 
-  return totalAmount;
+    // 1/12 процентной ставки
+    let percentRate = percent / (100 * 12);
+    //количество месяцев
+    let period = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth()); 
+    // ежемесячный платеж
+    let currentPay = (amount - contribution) * (percentRate + percentRate / ((Math.pow((1 + percentRate), period)) - 1)); 
+
+    let totalAmount = (currentPay * period).toFixed(2);
+
+    console.log(totalAmount);
+    return Number(totalAmount); 
 }
