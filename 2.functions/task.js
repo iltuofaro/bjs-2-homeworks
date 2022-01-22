931884d7d1c8aca42f3780d07056fed57a647e52
And8Alex <sand.dreeva@gmail.com>
2022-01-22 15:22:51 +0700

    task.js edited

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -9,9 +9,11 @@ function getArrayParams(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
       max = arr[i];
-    } if (arr[i] < min) {
+    } 
+    if (arr[i] < min) {
       min = arr[i];
-    } if (i < arr.length) {
+    } 
+    if (i < arr.length) {
     sum += arr[i];
     }
   }
