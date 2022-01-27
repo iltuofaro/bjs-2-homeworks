4dd642d2066555dd3939ab6ff67395a0b9ceec98
And8Alex <sand.dreeva@gmail.com>
2022-01-27 20:40:04 +0700

    task.js edited

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,31 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = true;
 
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length === arr2.length && arr1.every((item, idx) => arr1[idx] === arr2[idx])) {
+    return result;
+  } else {
+    
+    return false;
+  }
 }
+
+/*
+// вариант 2
+function compareArrays(arr1, arr2) {
+  return arr1.length === arr2.length && arr1.every((item, idx) => arr1[idx] === arr2[idx]);
+}
+*/
+
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((arr => arr > 0 && arr % 3 === 0)). map((arr => arr * 10));
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr;
 }
+
+/*
+// вариант 2
+function advancedFilter(arr) {
+  return arr.filter((arr => arr > 0 && arr % 3 === 0)). map((arr => arr * 10));
+}
+*/
