22ccc3e24a2d8b379267225088d45528215ecebc
And8Alex <sand.dreeva@gmail.com>
2022-04-10 14:41:19 +0700

    edited

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,4 +1,5 @@
 'use strict';
+// https://learn.javascript.ru/strict-mode
 
 function solveEquation(a, b, c) {
   let arr = [];
@@ -16,10 +17,26 @@ function solveEquation(a, b, c) {
   return arr;
 }
 
+// function solveEquation(a, b, c) {
+//   let arr;
+//   let d = Math.pow(b, 2) - (4*a*c);
+
+//   if (d < 0) {
+//     arr = [];
+//   } else if (d === 0) {
+//     arr = [-b / (2 * a)];
+//   } else {
+//     arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+//   };
+
+//   return arr;
+// }
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
   // контроль на корректность введенных данных
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
   if (isNaN(percent)) {
     return `Параметр "Процентная ставка" содержит некорректное значение: ${percent}`;
   } else if (isNaN(contribution)) {
